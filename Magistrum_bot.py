e6f20789599329b66d84988d79393591d1581207
Timaxacker <laztimofey@gmail.com>
2022-12-27 00:31:48 +0300

    add vector

--- Magistrum_bot.py
+++ Magistrum_bot.py
@@ -21,17 +21,17 @@ name_user = ''
 
 information = [] 
 
-bot_input = telebot.TeleBot('5009486880:AAE0gNDOLl2pouuA4x2it_3RQOGcV29D8vw') # API ключ бота
-bot_output = telebot.TeleBot('5095119695:AAFSPYO3Nz1HDqzr7njbSzxRG_cgpF1tQEY') # API ключ бота
+bot = telebot.TeleBot('5365169503:AAFFmQwmbkzjuCCLN1KSD1uCEBLI33xvGpk') # API ключ бота
+
     
 
-@bot_input.message_handler(commands=["start"]) # Команда для запуска бота 
+@bot.message_handler(commands=["start"]) # Команда для запуска бота 
 def start(m, res=False): # Функция срабатывающая при старте   
-    bot_input.send_message(m.chat.id, 'Вас приветствует Telegram бот детского роботехнического клуба "Магиструм"! Как к Вам обращаться?', reply_markup=markup) # Фраза встречающая пользователя после комманды /start
+    bot.send_message(m.chat.id, 'Вас приветствует Telegram бот детского роботехнического клуба "Магиструм"! Как к Вам обращаться?') # Фраза встречающая пользователя после комманды /start
 
 
-@bot_input.message_handler(content_types=["text"]) # Команда для получения текста 
-def name_user(m):
+@bot.message_handler(content_types=["text"]) # Команда для получения текста 
+def name_user_func(m):
     global answer, name_user
     
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -41,14 +41,15 @@ def name_user(m):
     name_user = m.text.strip()
     
     answer = name_user + ', нажимайте на кнопки для дальнейших действий, иначе я Вас не пойму'
-    bot_input.send_message(m.chat.id, answer, reply_markup=markup)
+    bot.send_message(m.chat.id, answer, reply_markup=markup)
+    bot.register_next_step_handler(m, menu)
     
   
 def menu(m): # Функция для обработки основных кнопок 
     global answer, channel
     
     if m.text.strip() == 'Зарегистрироваться':
-        answer = name_user + ', напишите, пожалуйста, имя и фамилию ребёнка'
+        answer = name_user + ', напишите, пожалуйста, фамилию и имя ребенка ребёнка'
         bot.send_message(m.chat.id, answer)
         bot.register_next_step_handler(m, kid_name)
         
@@ -62,27 +63,126 @@ def kid_name(m):
     global answer, information
     
     information.append(m.text.strip())
+
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1=types.KeyboardButton("4")
+    markup.add(item1)
+    item2=types.KeyboardButton("5")
+    markup.add(item2)
+    item3=types.KeyboardButton("6")
+    markup.add(item3)
+    item4=types.KeyboardButton("7")
+    markup.add(item4)
+    item5=types.KeyboardButton("8")
+    markup.add(item5)
+    item6=types.KeyboardButton("9")
+    markup.add(item6)
+    item7=types.KeyboardButton("10")
+    markup.add(item7)
+    item8=types.KeyboardButton("11")
+    markup.add(item8)
+    item9=types.KeyboardButton("12")
+    markup.add(item9)
+    item10=types.KeyboardButton("13")
+    markup.add(item10)
+    item11=types.KeyboardButton("14")
+    markup.add(item11)
+    item12=types.KeyboardButton("15")
+    markup.add(item12)
+    item13=types.KeyboardButton("16")
+    markup.add(item13)
+    item14=types.KeyboardButton("17")
+    markup.add(item14)
+    item15=types.KeyboardButton("18 и более")
+    markup.add(item15)
     
-    answer = name_user + ', напишите, пожалуйста, возраст ребёнка(целых лет, только цифры)'
-    bot.send_message(m.chat.id, answer)
-    bot.register_next_step_handler(m, kid_name)
+
+    answer = name_user + ', выберите, пожалуйста, возраст ребёнка'
+    bot.send_message(m.chat.id, answer, reply_markup=markup)
+    bot.register_next_step_handler(m, kid_age)
 
 
 def kid_age(m):
     global answer, information
     
-    if int(m.text.strip()) < 1:
-        answer = 'Вы указали возраст не корректно, попробуйте ещё раз'
+    if m.text.strip() == '4' or m.text.strip() == '5' or m.text.strip() == '6' or m.text.strip() == '7' or m.text.strip() == '8' or m.text.strip() == '9' or m.text.strip() == '10' or m.text.strip() == '11' or m.text.strip() == '12' or m.text.strip() == '13' or m.text.strip() == '14' or m.text.strip() == '15' or m.text.strip() == '16' or m.text.strip() == '17' or m.text.strip() == '18 и более':
+        information.append(int(m.text.strip()))
+        
+        if information[1] <= 6:
+            markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item1=types.KeyboardButton("Робототехника Lego WeDo 2.0")
+            markup.add(item1)
+
+        elif information[1] >= 10:
+            markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item1=types.KeyboardButton("Arduino")
+            markup.add(item1)
+            item2=types.KeyboardButton("Программирование на Python")
+            markup.add(item2)
+            item3=types.KeyboardButton("Геймдизайн")
+            markup.add(item3)
+            item4=types.KeyboardButton("Машинное обучение на Scratch")
+            markup.add(item4)
+            item5=types.KeyboardButton("3D-моделирование")
+            markup.add(item5)
+            item6=types.KeyboardButton("Робототехника Lego Mindstorms")
+            markup.add(item6)
+            item7=types.KeyboardButton("Scratch")
+            markup.add(item7)
+
+        elif information[1] >= 9:
+            markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item1=types.KeyboardButton("Программирование на Python")
+            markup.add(item1)
+            item2=types.KeyboardButton("Геймдизайн")
+            markup.add(item2)
+            item3=types.KeyboardButton("Машинное обучение на Scratch")
+            markup.add(item3)
+            item4=types.KeyboardButton("3D-моделирование")
+            markup.add(item4)
+            item5=types.KeyboardButton("Робототехника Lego Mindstorms")
+            markup.add(item5)
+            item6=types.KeyboardButton("Scratch")
+            markup.add(item6)
+
+        elif information[1] >= 8:
+            markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item1=types.KeyboardButton("3D-моделирование")
+            markup.add(item1)
+            item2=types.KeyboardButton("Робототехника Lego Mindstorms")
+            markup.add(item2)
+            item3=types.KeyboardButton("Scratch")
+            markup.add(item3)
+
+        elif information[1] >= 7:
+            markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item1=types.KeyboardButton("Робототехника Lego Mindstorms")
+            markup.add(item1)
+            item2=types.KeyboardButton("Scratch")
+            markup.add(item2)
+
+        elif information[1] >= 6:
+            markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item1=types.KeyboardButton("Scratch")
+            markup.add(item1)
+        
+
+        answer = name_user + ', выберите, пожалуйста, направление занятий. Если в предложенных вариантах нет желаемого Вами направления, то уточните этот вопрос с администратором центра (контактные данные)'
+        bot.send_message(m.chat.id, answer, reply_markup=markup)
+        bot.register_next_step_handler(m, vector)
+        
+    else:
+        answer = "Нажимайте, пожалуйста, на кнопки, иначе я Вас не понимаю!"
         bot.send_message(m.chat.id, answer)
         bot.register_next_step_handler(m, kid_age)
-    elif int(m.text.strip()) < 4 or int(m.text.strip()) > 17:
-        answer = 'К сожалению, для указанного возраста нет подходящего направления. Ближайшее к вашему возрасту занятие - Lego WeDo (4+ лет)'
+        
+
+def vector(m):
+    global answer, information
+
     information.append(m.text.strip())
+
+    print(information)
     
-    answer = name_user + ', напишите, пожалуйста, возраст ребёнка'
-    bot.send_message(m.chat.id, answer)
-    bot.register_next_step_handler(m, kid_name)
-    
-    
-bot_input.polling(none_stop=True, interval=0) # Запуск бота 
-bot_output.polling(none_stop=True, interval=0) # Запуск бота 
+
+bot.polling(none_stop=True, interval=0) # Запуск бота 
