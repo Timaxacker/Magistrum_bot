fc693911f8dd81302e4149f72523f2a4e9e5d5fa
Timaxacker <laztimofey@gmail.com>
2024-10-31 00:06:10 +0300

    clear code

--- Magistrum_bot.py
+++ Magistrum_bot.py
@@ -12,19 +12,15 @@ exch.bot = bot
 connection = DBMS.create_connection(f"{sys.path[0]}/database.sqlite")
 
 # DBMS.execute_query(connection, DBMS.create_lessons_table)
-# DBMS.execute_query(connection, DBMS.create_teachers_table)
-# DBMS.execute_query(connection, DBMS.create_areas_table)
-# DBMS.execute_query(connection, DBMS.create_type_lesson_table)
-# DBMS.execute_query(connection, DBMS.create_state_teacher_table)
-# DBMS.execute_query(connection, DBMS.create_state_area_table)
-# DBMS.execute_query(connection, DBMS.create_comments_table)
-
+DBMS.execute_query(connection, DBMS.addition_comment)
 # DBMS.execute_query(connection, DBMS.delete)
 
 answer = None
+
 calendar = Calendar(language=RUSSIAN_LANGUAGE)
 calendar_1 = CallbackData('calendar_1', 'action', 'year', 'month', 'day')
 now = datetime.now()
+
 
 @exch.propperWrapper()
 @bot.message_handler(commands=["start"])
@@ -41,13 +37,50 @@ def start(m, res=False):
     return True
 
 
+#@exch.propperWrapper()
+#def passowrd(m):
+
+
+
 @exch.propperWrapper()    
 def menu(m):
     if m.text.strip() == "Расписание":
         answer =  "Выберите дату"
         bot.send_message(m.chat.id, answer, reply_markup=calendar.create_calendar(name=calendar_1.prefix, year=now.year, month=now.month))
         
+    elif m.text.strip() == "Корректировать":
+        """
+        answer = "Новое время начала"
+        bot.send_message(m.chat.id, answer)
+        bot.register_next_step_handler(m, rasp)
+        """
+
+
+    elif m.text.strip() == "Неотмеченные":
+        """
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        lst_of_but = ["Расписание", "Корректировать", "Неотмеченные"]
+        for i in lst_of_but:
+            markup.add(types.KeyboardButton(i))
+        
+        
+        answer = "Сообщение отправлено"
+        bot.send_message(m.chat.id, answer, reply_markup=markup)
+        bot.send_message(1835294966, "Отметится!")
+        bot.register_next_step_handler(m, menu)
+        """
+
+
+
+    else:
+        answer = "Нажимай на кнопки!"
+        bot.send_message(m.chat.id, answer)
+        bot.register_next_step_handler(m, menu)
+
+        
+
     return True
+
 
 @exch.propperWrapper()
 @bot.callback_query_handler(func=lambda call: call.data.startswith(calendar_1.prefix))
@@ -57,20 +90,54 @@ def callback_inline(call: types.CallbackQuery):
 
     if action == 'DAY':
         bot.send_message(chat_id=call.from_user.id, text=f'Вы выбрали {date.strftime("%d.%m.%Y")}', reply_markup=types.ReplyKeyboardRemove())
+        
+        """
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        lst_of_but = ["Расписание", "Корректировать", "Неотмеченные"]
+        for i in lst_of_but:
+            markup.add(types.KeyboardButton(i))
+
+
+        answer = f"21 октября понедельник\nМагиструм:\n@Timahacker - {time} - 13:30 Python"
+        bot.send_message(call.message.chat.id, answer, reply_markup=markup)
+
+        dayy = date.strftime("%d")
+        print(dayy)
+        bot.register_next_step_handler(call.message, menu)
+        """
+
+
+
 
     elif action == 'CANCEL':
         markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-        lst_of_but = ["Расписание"]
+        lst_of_but = ["Расписание", "Корректировать", "Неотмеченные"]
         for i in lst_of_but:
             markup.add(types.KeyboardButton(i))
 
         bot.send_message(chat_id=call.from_user.id, text='Отмена', reply_markup=markup)
-        bot.register_next_step_handler(call, menu)  # call.message
+        bot.register_next_step_handler(call.message, menu)  # call.message
 
     return True
 
+"""
+@exch.propperWrapper()    
+def rasp(m):   
+    global time
+    time = m.text.strip()
+    
+    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+    lst_of_but = ["Расписание", "Корректировать", "Неотмеченные"]
+    for i in lst_of_but:
+        markup.add(types.KeyboardButton(i))
 
 
+    answer = "Новое время установленно"
+    bot.send_message(m.chat.id, answer,  reply_markup=markup)
+    bot.register_next_step_handler(m, menu)
+
+    return True
+"""
 
 
 while True:
